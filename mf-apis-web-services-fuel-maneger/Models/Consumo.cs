a4768f8cf236402b4edc1b3fe158d56dbea4810a
Nekiol <nekiol@hotmail.com>
2023-03-28 23:27:48 -0300

    Criação e configuração dos modelos de Veículo e Consumo

---
+++ mf-apis-web-services-fuel-maneger/Models/Consumo.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace mf_apis_web_services_fuel_maneger.Models
+{
+    [Table("Consumo")]
+    public class Consumo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Descricao { get; set; }
+        [Required]
+        public DateTime Data { get; set; }
+        [Required]
+        public string Valor { get; set;}
+        [Required]
+        public  tipoCombustivel Tipo { get; set;}
+
+        [Required]
+        public int VeiculoId { get;set;}
+
+        public Veiculo Veiculo { get; set; }
+
+    }
+
+    public enum tipoCombustivel
+    {
+        Diesel,
+        Etanol,
+        Gasolina
+    }
+}
